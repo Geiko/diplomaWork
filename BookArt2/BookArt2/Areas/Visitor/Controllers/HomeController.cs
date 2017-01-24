5f3e1376401e2cdcf8d863e6e649cccbd03dd546
Geiko <k.i.geiko@gmail.com>
2017-01-24 21:35:10 +0200

    new2

---
+++ BookArt2/BookArt2/Areas/Visitor/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookArt2.Areas.Visitor.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Visitor/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookArt2/BookArt2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookArt2.Startup))]
+namespace BookArt2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
